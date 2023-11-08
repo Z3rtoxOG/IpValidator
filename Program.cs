2abd596e3febc21dbd90a035412f4cec7167e837
Z3rtoxOG <57806785+Z3rtox@users.noreply.github.com>
2023-11-09 00:40:10 +0100

    1.0

--- Program.cs
+++ Program.cs
@@ -1,50 +1,49 @@
-﻿using System;
-
+﻿
 class Program
 {
     static void Main(string[] args)
     {
-        string MyIp;
-        Console.WriteLine("podaj twoje IP");
-        MyIp = Console.ReadLine();
 
-        if (Validator(MyIp))
+        Console.WriteLine("podaj twoje IP");
+        string myIp = Console.ReadLine();
+
+        //wynik 
+        if (Validator(myIp))
         {
-            Console.WriteLine($"Twoje ip {MyIp} jest poprawne");
+            Console.WriteLine($"Twoje ip {myIp} jest poprawne");
         }
         else
         {
-            Console.WriteLine($"Twoje ip {MyIp} nie  jest poprawne");
+            Console.WriteLine($"Twoje ip {myIp} nie  jest poprawne");
         }
     }
     static bool Validator(string ipadress)
     {
-        var ipParts = ipadress.Split(".");
-
-        if (ipParts.Length != 4)
+        //sprawdzenie czy ip nie jest równe adresom zarezerwowanym dla masek
+        if (ipadress == "0.0.0.0" || ipadress == "255.255.255.255")
         {
             return false;
         }
 
-        foreach (var part in ipParts)
+        //dzielimy nasz ciąg na 4 części według standardu IPV4
+        var parts = ipadress.Split(".");
+
+        if (parts.Length != 4) { return false; }
+
+        // validacja ip v4 nie zaczyna się od 0
+        foreach (var part in parts)
         {
-            if (part.StartsWith("0") && part.Length > 1)
-            {
-                return false;
-            }
-
-            if (part.Length != part.Trim().Length)
-            {
-                return false;
-            }
-
+            if (part.StartsWith("0") && part.Length > 1) { return false; }
+            //sprawdzam czy występują spacje w podanym IP 
+            if (part.Length != part.Trim().Length) { return false; }
             int number;
             var result = Int32.TryParse(part, out number);
-
+            // nie pozwalamy na wejscie cyf poniżej 0 i powyżej 255
             if (!result || number > 255 || number < 0)
             {
                 return false;
             }
+
         }
 
         return true;
